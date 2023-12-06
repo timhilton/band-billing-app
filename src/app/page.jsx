a6d57578c422e1cb26b45653d41aed211419ae54
Tim Hilton <tim@timhilton.net>
2023-12-06 01:47:04 -0800

    trying and trying and god damn

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react'
 
 export default function Home() {
   const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID
-  const REDIRECT_URI = 'https://band-billing-app-timhilton.vercel.app';
+  let REDIRECT_URI = process.env.NODE_ENV === 'production' ? 'https://band-billing-app-timhilton.vercel.app' : 'http://localhost:3000';
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
   const RESPONSE_TYPE = "token"
 
